89cab34817be806f978563997908a4db8b81dda3
Alex Fradera <alex.fradera@gmail.com>
2021-06-21 18:11:04 +0100

    Continuing to functionise the code but hitting issues with network analysis. May need to time travel to see if it worked in the past.

--- SR_functions.R
+++ SR_functions.R
@@ -2,6 +2,8 @@
 library(litsearchr)
 library(tools)
 
+
+# Collects all the .nbib files in the wd and outputs them as dataframes
 fileNames <- Sys.glob("*.nbib")
 for (fileName in fileNames) {
   sample <- import_results(file=fileName)
@@ -10,17 +12,35 @@ for (fileName in fileNames) {
 
 
   
-  
-    # read old data:
-  sample <- read.csv(fileNames[fileNumber],
-                     header = TRUE,
-                     sep = ",")
 
-grab_search <- function(filename,objectname) {
-  
-   objectname <- import_results(file=filename)
+
+
+# firstly a short function to redescribe proportions as percentages
+term_extractor<- function(x, min_length, min_repeat) {
+   extract_terms(keywords=x[, "keywords"], method="tagged", min_n=min_length, min_freq = min_repeat)
 }
-  
-grab_search("test.nbib",search_1)  
-nrow(search_1)
-  
+
+
+# getting keywords out of the title 
+get_best_titles<- function(data){
+extract_terms(
+  text=data[, "title"],
+  method="fakerake",
+  min_freq=3, min_n=2,
+  stopwords=all_stopwords)
+}
+
+
+create_network < function(data, minimum_studies){
+  docs <- paste(data[, "title"], data[, "abstract"])
+  dfm <- create_dfm(elements=docs, features=terms)
+  create_network(dfm, min_studies=minimum_studies)
+}
+
+
+```{r create_network}
+docs <- paste(naive_results[, "title"], naive_results[, "abstract"])
+dfm <- create_dfm(elements=docs, features=terms)
+g <- create_network(dfm, min_studies=1)
+
+```
