30b84216b6a69af89bab44a5d4959c9c600e8fd6
Alex Fradera <alex.fradera@gmail.com>
2021-06-21 19:24:15 +0100

    WOrked through Rmd up to line 103.

--- SR_functions.R
+++ SR_functions.R
@@ -31,16 +31,48 @@ extract_terms(
 }
 
 
-create_network < function(data, minimum_studies){
+create_the_network <- function(data, terms){
   docs <- paste(data[, "title"], data[, "abstract"])
   dfm <- create_dfm(elements=docs, features=terms)
-  create_network(dfm, min_studies=minimum_studies)
+  g <- create_network(dfm, min_studies=1)
+
+}
+
+print_the_network <-function(g){
+  network_vis<-ggraph(g, layout="stress") +
+    coord_fixed() +
+    expand_limits(x=c(-3, 3)) +
+    geom_edge_link(aes(alpha=weight)) +
+    geom_node_point(shape="circle filled", fill="white") +
+    geom_node_text(aes(label=name), hjust="outward", check_overlap=TRUE) +
+    guides(edge_alpha=FALSE)
+  network_vis  
 }
 
 
-```{r create_network}
-docs <- paste(naive_results[, "title"], naive_results[, "abstract"])
-dfm <- create_dfm(elements=docs, features=terms)
-g <- create_network(dfm, min_studies=1)
+strengths_of_network <- function(g){
+  strengths <- strength(g)
+  data.frame(term=names(strengths), strength=strengths, row.names=NULL) %>%
+    mutate(rank=rank(strength, ties.method="min")) %>%
+    arrange(strength) ->
+    term_strengths
+  
+  term_strengths
+}
 
-```
+
+
+find_network_cutoffs <- function(termstrengths,network){
+  cutoff_fig <- ggplot(termstrengths, aes(x=rank, y=strength, label=term)) +
+    geom_line() +
+    geom_point() +
+    geom_text(data=filter(termstrengths, rank>5), hjust="right", nudge_y=20, check_overlap=TRUE)
+  
+  cutoff_change <- find_cutoff(network, method="changepoint", knot_num=3)
+  
+  cutoff_fig +
+    geom_hline(yintercept=cutoff_change, linetype="dashed")
+  
+}
+
+
